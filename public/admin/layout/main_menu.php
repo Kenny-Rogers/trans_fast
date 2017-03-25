5fbbfb6a5817da1fbb26b9b664ed1a7d7288d8e2
Kenny-Rogers <ylodonu@gmail.com>
2017-03-25 05:57:09 +0000

    adding icons

--- public/admin/layout/main_menu.php
+++ public/admin/layout/main_menu.php
@@ -3,75 +3,35 @@
       <!-- sidebar menu start-->
       <ul class="sidebar-menu">
           <li class="active">
-              <a class="" href="index.html">
-                  <i class="icon_house_alt"></i>
+              <a class="" href="#">
+                  <i class="fa fa-home"></i>
                   <span>Dashboard</span>
               </a>
           </li>
-  <li class="sub-menu">
-              <a href="javascript:;" class="">
-                  <i class="icon_document_alt"></i>
-                  <span>Forms</span>
-                  <span class="menu-arrow arrow_carrot-right"></span>
-              </a>
-              <ul class="sub">
-                  <li><a class="" href="form_component.html">Form Elements</a></li>
-                  <li><a class="" href="form_validation.html">Form Validation</a></li>
-              </ul>
-          </li>
-          <li class="sub-menu">
-              <a href="javascript:;" class="">
-                  <i class="icon_desktop"></i>
-                  <span>UI Fitures</span>
-                  <span class="menu-arrow arrow_carrot-right"></span>
-              </a>
-              <ul class="sub">
-                  <li><a class="" href="general.html">Elements</a></li>
-                  <li><a class="" href="buttons.html">Buttons</a></li>
-                  <li><a class="" href="grids.html">Grids</a></li>
-              </ul>
-          </li>
           <li>
-              <a class="" href="widgets.html">
-                  <i class="icon_genius"></i>
-                  <span>Widgets</span>
+              <a class="" href="#">
+                  <i class="fa fa-file" aria-hidden="true"></i>
+                  <span>View order</span>
               </a>
           </li>
           <li>
-              <a class="" href="chart-chartjs.html">
-                  <i class="icon_piechart"></i>
-                  <span>Charts</span>
-
+              <a class="" href="#">
+                  <i class="fa fa-tasks" aria-hidden="true"></i>
+                  <span>Process Order</span>
               </a>
-
           </li>
-
-          <li class="sub-menu">
-              <a href="javascript:;" class="">
-                  <i class="icon_table"></i>
-                  <span>Tables</span>
-                  <span class="menu-arrow arrow_carrot-right"></span>
+          <li>
+              <a class="" href="#">
+                  <i class="fa fa-envelope" aria-hidden="true"></i>
+                  <span>Deliver Transcript</span>
               </a>
-              <ul class="sub">
-                  <li><a class="" href="basic_table.html">Basic Table</a></li>
-              </ul>
           </li>
-
-          <li class="sub-menu">
-              <a href="javascript:;" class="">
-                  <i class="icon_documents_alt"></i>
-                  <span>Pages</span>
-                  <span class="menu-arrow arrow_carrot-right"></span>
+          <li>
+              <a class="" href="#">
+                  <i class="fa fa-bar-chart"></i>
+                  <span>Generate Report</span>
               </a>
-              <ul class="sub">
-                  <li><a class="" href="profile.html">Profile</a></li>
-                  <li><a class="" href="login.html"><span>Login Page</span></a></li>
-                  <li><a class="" href="blank.html">Blank Page</a></li>
-                  <li><a class="" href="404.html">404 Error</a></li>
-              </ul>
           </li>
-
-      </ul>
       <!-- sidebar menu end-->
   </div>
 </aside>
