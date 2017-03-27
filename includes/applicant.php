d793ab65c166664f509a5155fc42b7d4a01defee
Kenny-Rogers <ylodonu@gmail.com>
2017-03-27 15:26:20 +0000

    class applicant created

---
+++ includes/applicant.php
@@ -0,0 +1,37 @@
+<?php
+//applicant for a transcript
+class Applicant{
+  protected $tablename;
+  protected $db_fields;
+  private $school_id;
+  private $first_name;
+  private $last_name;
+  private $middle_name;
+  private $email;
+  private $telephone;
+  private $mode_of_delivery;
+  private $pace_of_processing;
+  private $date_of_application;
+  private $date_delivered;
+
+  //initialise variables
+  private init(){
+    $this->school_id = 0;
+    $this->first_name = "";
+    $this->last_name = "";
+    $this->middle_name = "";
+    $this->email = "";
+    $this->telephone = "";
+    $this->mode_of_delivery = "";
+    $this->pace_of_processing = "";
+    $this->date_of_application = "";
+    $this->date_delivered = "";
+  }
+
+  //constructor
+  public function Applicant(){
+    //TODO:: continue
+  }
+
+}
+?>
