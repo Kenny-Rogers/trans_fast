67e53f588036aeb33db813f6cd3eacb3bb8c1b0e
Kenny-Rogers <ylodonu@gmail.com>
2017-03-27 14:44:25 +0000

    changing $page to $display

--- includes/page_loader.php
+++ includes/page_loader.php
@@ -1,16 +1,37 @@
 <?php
   //IF first time::get the page clicked on
   //ELSE :: set to default
+  $display = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
   $view = "";
-  $page = isset($_GET['page']) ? $_GET['page'] : 'dashboard';
 
-  switch ($page) {
+  switch ($display) {
+
+    case 'view_order':
+      //display display_order page
+      $view = "pages/view_order/display_orders.php";
+      break;
+
+    case 'process_order':
+      //display process_order page
+      $view = "pages/process/process_order.php";
+      break;
+
+    case 'generate':
+      //display summary report
+      $view = "pages/generate/summary.php";
+      break;
+
+    case 'deliver':
+      $view = "pages/deliver/deliver_trans.php";
+      break;
+
     case 'dashboard':
-      $view="template.php";
+      //display summary report
+      $view = "pages/generate/summary.php";
       break;
 
     default:
-      $view="template.php";
+      $view = "pages/generate/summary.php";
       break;
   }
 ?>
