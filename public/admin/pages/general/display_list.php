7fe6285409e94259a5c9a89671b1dbef78d0a863
Kenny-Rogers <ylodonu@gmail.com>
2017-04-01 17:44:19 +0000

    displays all tables

---
+++ public/admin/pages/general/display_list.php
@@ -0,0 +1,19 @@
+<?php
+$val = isset($_GET['query1'])?$_GET['query1']:'Null';
+echo "{$val}";
+?>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Date Applied</th>
+        <th>Delivery Point</th>
+      </tr>
+    </thead>
+    <tbody>
+    <?php //TODO::code to display rows of the data ?>
+  </tbody>
+  </table>
+</div>
