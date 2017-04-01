5784932a9895d73f152b852e39221c0b7d0c7e8e
Kenny-Rogers <ylodonu@gmail.com>
2017-04-01 19:05:47 +0000

    contains commonly used functions

---
+++ includes/functions.php
@@ -0,0 +1,48 @@
+<?php
+function strip_zeros_from_date($date=""){
+  //removes marked zeros
+  $no_zeros=str_replace('*0', '', $date);
+  //removes any remaining marks
+  $cleaned_string=str_replace('*', '', $no_zeros);
+  return $cleaned_string;
+}
+
+function redirect_to($location=NULL){
+  //redirects to a given page
+  if($location!=NULL){
+    header("Location:{$location}");
+    exit;
+  }
+}
+
+function output_message($message=""){
+  //displays a paragraphed text
+  if(!empty($message)){
+    return "<p class=\"message\">{$message}</p>";
+  }else{
+    return "";
+  }
+}
+
+function __autoload($class_name){
+    //this function handles classes that haven't being included
+    $class_name=strtolower($class_name);
+    $path=LIB_PATH.DS."{$class_name}.php";
+    if(file_exists($path)){
+      require_once($path);
+    }else{
+      die("The file {$class_name}.php could not be found");
+    }
+  }
+
+  function include_layout_template($template=""){
+    //includes any html header or footer template sent as arguments
+    include(SITE_ROOT.DS.'public'.DS.'layouts'.DS.$template);
+  }
+
+function datetime_to_text($datetime=""){
+  //displays the date in a different format
+  $unixdatetime=strtotime($datatime);
+  return Strftime("%B %d, %Y at %I:%M %p", $unixdatetime);
+}
+?>
