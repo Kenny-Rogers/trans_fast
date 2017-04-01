32316cfaf34f0db6072a64ef9e18539c01058359
Kenny-Rogers <ylodonu@gmail.com>
2017-04-01 18:16:48 +0000

    allowing for title text and title icon change

--- public/admin/layout/main_page.php
+++ public/admin/layout/main_page.php
@@ -3,11 +3,11 @@
          <!--overview start-->
   <div class="row">
   <div class="col-lg-12">
-    <h3 class="page-header"><i class="fa fa-laptop"></i> Dashboard</h3>
-    <ol class="breadcrumb">
-      <li><i class="fa fa-home"></i><a href="index.html">Home</a></li>
+    <h3 class="page-header"><i class="<?php echo "{$title_class}";?>"></i><?php echo "{$page_title}";?></h3>
+    <!--ol class="breadcrumb">
+      <li><i class="fa fa-home"></i><a href="#">Home</a></li>
       <li><i class="fa fa-laptop"></i>Dashboard</li>
-    </ol>
+    </ol-->
   </div>
 </div>
       <!-- displays the requested page -->
