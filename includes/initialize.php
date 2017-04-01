9ee1597f2098eb2f3c5aaeaa04c6de6d6b942325
Kenny-Rogers <ylodonu@gmail.com>
2017-04-01 19:06:14 +0000

    adds all the necessary files

---
+++ includes/initialize.php
@@ -0,0 +1,34 @@
+<?php
+//defines the core paths to allow for the require_once to work
+//DIRECTORY_SEPERATOR a pre-defined constant  (\ for windows , / for unix)
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+//SITE_ROOT::defines the file system path to the project
+//must be changed if on a different machine
+defined('SITE_ROOT')?null:
+define('SITE_ROOT', DS.'var'.DS.'www'.DS.'html'.DS.'photo_gallery');
+
+//LIB_PATH::defines the path to the includes folder which contains all
+//the libraries
+defined('LIB_PATH')?null:define('LIB_PATH', SITE_ROOT.DS.'includes');
+
+
+//load the config file first
+require_once(LIB_PATH.DS."config.php");
+
+//load basic functions next so that the rest of the loaded files can
+//uses these two (config.php and functions.php)
+require_once(LIB_PATH.DS.'functions.php');
+
+//load core objects
+require_once(LIB_PATH.DS.'session.php');
+require_once(LIB_PATH.DS.'database.php');
+require_once(LIB_PATH.DS.'database_object.php');
+require_once(LIB_PATH.DS.'log.php');
+require_once(LIB_PATH.DS.'pagination.php');
+
+//load database-related classes
+require_once(LIB_PATH.DS.'user.php');
+require_once(LIB_PATH.DS.'photograph.php');
+
+?>
