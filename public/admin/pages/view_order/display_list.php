44dc71a15ba2304440a6362cff842eb31e19f84b
Kenny-Rogers <ylodonu@gmail.com>
2017-04-01 17:30:46 +0000

    adding the table structure

--- public/admin/pages/view_order/display_list.php
+++ public/admin/pages/view_order/display_list.php
@@ -2,3 +2,16 @@
 $val = isset($_GET['query1'])?$_GET['query1']:'Null';
 echo "{$val}";
 ?>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Date Applied</th>
+        <th>Delivery Point</th>
+      </tr>
+    </thead>
+    <?php //TODO::code to display rows of the data ?>
+  </table>
+</div>
