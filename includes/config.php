49b6401730703d0d0de9e1a6aa6e90a1a362b912
Kenny-Rogers <ylodonu@gmail.com>
2017-04-02 12:29:29 +0000

    changing db login details

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 //db constants
 defined('DB_SERVER')? null : define("DB_SERVER", "localhost");
-defined('DB_USER')? null : define("DB_USER", "gallery");
-defined('DB_PASS')? null : define("DB_PASS", "jackie");
-defined('DB_NAME')? null : define("DB_NAME", "trans90fast");
+defined('DB_USER')? null : define("DB_USER", "jackie");
+defined('DB_PASS')? null : define("DB_PASS", "trans90fast");
+defined('DB_NAME')? null : define("DB_NAME", "trans_fast");
 ?>
