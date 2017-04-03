7fd35b30ed9a7a9b9c90acc533b6abff8609d040
Kenny-Rogers <ylodonu@gmail.com>
2017-04-03 08:05:10 +0000

    including changing active tabs

--- public/admin/layout/main_menu.php
+++ public/admin/layout/main_menu.php
@@ -2,31 +2,31 @@
   <div id="sidebar"  class="nav-collapse ">
       <!-- sidebar menu start-->
       <ul class="sidebar-menu">
-          <li class="active">
+          <li <?php echo $class_da; ?>>
               <a class="" href="?page=dashboard">
                   <i class="fa fa-home" aria-hidden="true"></i>
                   <span>Dashboard</span>
               </a>
           </li>
-          <li>
+          <li <?php echo $class_v; ?>>
               <a class="" href="?page=view_order">
                   <i class="fa fa-file" aria-hidden="true"></i>
                   <span>View order</span>
               </a>
           </li>
-          <li>
+          <li <?php echo $class_p; ?>>
               <a class="" href="?page=process_order">
                   <i class="fa fa-tasks" aria-hidden="true"></i>
                   <span>Process Order</span>
               </a>
           </li>
-          <li>
+          <li <?php echo $class_d; ?>>
               <a class="" href="?page=deliver">
                   <i class="fa fa-envelope" aria-hidden="true"></i>
                   <span>Deliver Transcript</span>
               </a>
           </li>
-          <li>
+          <li <?php echo $class_g; ?>>
               <a class="" href="?page=generate">
                   <i class="fa fa-bar-chart"></i>
                   <span>Generate Report</span>
