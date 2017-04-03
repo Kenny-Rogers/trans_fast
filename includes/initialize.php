cba8788ba3f5f9aea87791fed429ced7ee69da2d
Kenny-Rogers <ylodonu@gmail.com>
2017-04-03 07:57:59 +0000

    adding applicant class

--- includes/initialize.php
+++ includes/initialize.php
@@ -6,7 +6,7 @@ defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
 //SITE_ROOT::defines the file system path to the project
 //must be changed if on a different machine
 defined('SITE_ROOT')?null:
-define('SITE_ROOT', DS.'var'.DS.'www'.DS.'html'.DS.'photo_gallery');
+define('SITE_ROOT', DS.'var'.DS.'www'.DS.'html'.DS.'trans_fast');
 
 //LIB_PATH::defines the path to the includes folder which contains all
 //the libraries
@@ -21,14 +21,15 @@ require_once(LIB_PATH.DS."config.php");
 require_once(LIB_PATH.DS.'functions.php');
 
 //load core objects
-require_once(LIB_PATH.DS.'session.php');
+//require_once(LIB_PATH.DS.'session.php');
 require_once(LIB_PATH.DS.'database.php');
 require_once(LIB_PATH.DS.'database_object.php');
-require_once(LIB_PATH.DS.'log.php');
-require_once(LIB_PATH.DS.'pagination.php');
+// require_once(LIB_PATH.DS.'log.php');
+// require_once(LIB_PATH.DS.'pagination.php');
 
 //load database-related classes
-require_once(LIB_PATH.DS.'user.php');
-require_once(LIB_PATH.DS.'photograph.php');
+require_once(LIB_PATH.DS.'models'.DS.'applicant.php');
+// require_once(LIB_PATH.DS.'user.php');
+// require_once(LIB_PATH.DS.'photograph.php');
 
 ?>
