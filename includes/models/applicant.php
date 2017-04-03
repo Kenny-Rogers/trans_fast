713826c787d11d21a30cdb0f958e96deafc0ab03
Kenny-Rogers <ylodonu@gmail.com>
2017-04-03 07:57:32 +0000

    updating applicant class

--- includes/models/applicant.php
+++ includes/models/applicant.php
@@ -1,12 +1,12 @@
 <?php
 //applicant Info
-class Applicant{
-  protected $table_name = "admin";
-  protected $db_fields =
+class Applicant extends DatabaseObject{
+  protected static $table_name = "admin";
+  protected static $db_fields =
     array("school_id", "first_name", "last_name", "password", "email",
           "telephone", "mode_of_delivery", "pace_of_processing", "date_applied",
-          "date_delivered");
-  protected $school_id;
+          "date_delivered", "status");
+  public $school_id;
   protected $first_name;
   protected $last_name;
   protected $middle_name;
@@ -17,6 +17,7 @@ class Applicant{
   protected $pace_of_processing;
   protected $date_applied;
   protected $date_delivered;
+  protected $status;
 
   public static function authenticate($email="", $password=""){
     //validates username and password
@@ -65,6 +66,45 @@ class Applicant{
     }else {
       return "";
     }
+  }
+
+  public static function get_list_by($check1 = "", $check2 = ""){
+    //query string
+    $sql = "";
+    if (( $check1 == "paid" ) && (( $check2 == "express" ) || ( $check2 == "normal" ))){
+      //for PAID Applicants EXPRESS OR NORMAL
+      $sql = "SELECT * FROM applicant WHERE Id IN (SELECT paid_by FROM payment)
+              AND pace_of_processing='$check2'";
+    } elseif (( $check1 == "not_paid" ) && (( $check2 == "express" ) || ( $check2 == "normal" ))){
+      //for NOT_PAID Applicants EXPRESS OR NORMAL
+      $sql = "SELECT * FROM applicant WHERE Id NOT IN (SELECT paid_by FROM payment)
+              AND pace_of_processing='$check2'";
+    } elseif (( $check2 == "not_paid" ) && (( $check1 == "express" ) || ( $check1 == "normal" ))){
+      //for EXPRESS OR NORMAL Applicants NOT_PAID
+      $sql = "SELECT * FROM applicant WHERE Id NOT IN (SELECT paid_by FROM payment)
+              AND pace_of_processing='$check1'";
+    } elseif (( $check2 == "paid" ) && (( $check1 == "express" ) || ( $check1 == "normal" ))){
+      //for EXPRESS OR NORMAL Applicants NOT_PAID
+      $sql = "SELECT * FROM applicant WHERE Id IN (SELECT paid_by FROM payment)
+              AND pace_of_processing='$check1'";
+    } elseif (( $check2 == "Null" ) && (( $check1 == "express" ) || ( $check1 == "normal" ))){
+      //for EXPRESS OR NORMAL Applicants
+      $sql = "SELECT * FROM applicant WHERE pace_of_processing='$check1'";
+    } elseif (( $check1 == "paid" ) && ( $check2 == "Null" )){
+      //for PAID Applicants
+      $sql = "SELECT * FROM applicant WHERE Id IN (SELECT paid_by FROM payment)";
+    } elseif (( $check1 == "not_paid" ) && ( $check2 == "Null" )){
+      //for  NOT_PAID Applicants
+      $sql = "SELECT * FROM applicant WHERE Id NOT IN (SELECT paid_by FROM payment)";
+    } elseif ((( $check1 == "ems" ) || ( $check1 == "pick_up" ) || ( $check1 == "email" )) && ( $check2 == "Null" )){
+      //for various delivery types
+      $sql = "SELECT * FROM applicant WHERE Id IN (SELECT appl_id FROM receipient WHERE mode_of_delivery='$check1')";
+    } else {
+      //default
+      $sql = "SELECT * FROM applicant ";
+    }
+
+    return $sql;
   }
 }
 ?>
