4a1dd39d8ae1b3a946323dfe2b91e7063da32a23
Kenny-Rogers <ylodonu@gmail.com>
2017-04-03 10:46:22 +0000

    class to hold recipient info

---
+++ includes/models/recipient.php
@@ -0,0 +1,24 @@
+<?php
+//class for Recipients
+class Recipient{
+  protected static $table_name = "recipient";
+  protected static $db_fields =
+    array("receip_id", "appl_id", "email", "name", "mode_of_delivery", "telephone",
+          "region", "address", "country", "pcode", "zcode", "fax", "status");
+  protected $receip_id;
+  protected $appl_id;
+  protected $email;
+  protected $name;
+  protected $mode_of_delivery;
+  protected $telephone;
+  protected $region;
+  protected $address;
+  protected $country;
+  protected $pcode;
+  protected $zcode;
+  protected $fax;
+  protected $status;
+
+
+}
+?>
