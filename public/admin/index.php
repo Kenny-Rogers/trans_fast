cb9e720195685cf5c2d11f943553f8aa77ec694f
Kenny-Rogers <ylodonu@gmail.com>
2017-04-04 12:44:38 +0000

    adding login checks

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,11 +1,12 @@
 <?php include('../../includes/page_loader.php'); ?>
+//if(!$session->is_logged_in()){redirect_to('login.php');}
 <?php include('layout/header.php'); ?>
 <body>
  <!-- container section start -->
  <section id="container" class="">
 
   <!--header end-->
-  <?php include('layout/navbar.php') ?>
+  <?php include('layout/navbar.php'); ?>
   <!--header end-->
 
   <!--sidebar start-->
