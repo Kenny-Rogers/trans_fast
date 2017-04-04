052db1b937e2c82957a9710e975cd7e74041c22e
Kenny-Rogers <ylodonu@gmail.com>
2017-04-04 12:43:45 +0000

    adding session variable for current page to restrict table columns

--- public/admin/pages/general/display_list.php
+++ public/admin/pages/general/display_list.php
@@ -1,15 +1,18 @@
-<?php require_once('../../../../includes/initialize.php');
+<?php session_start();
+require_once('../../../../includes/initialize.php');
 //if(!$session->is_logged_in()){redirect_to('login.php');}
 //values passed  for check {paid or not_paid} {normal or express}
 //{ems or pick_up or email}
 $check1 = isset($_GET['query1'])?$_GET['query1']:'Null';
 $check2 = isset($_GET['query2'])?$_GET['query2']:'Null';
+$display = isset($_SESSION['display']) ? $_SESSION['display'] : 'dashboard';
 // echo "check1: {$check1}<br>";
 // echo "check2: {$check2}<br>";
-
+// echo "display: {$display}<br>";
 //query string;
 $sql = Applicant::get_list_by($check1, $check2);
-
+//next page
+$next_page = Applicant::find_next_page($check1, $check2);
 //getting list of applicant objects from the db
 $applicants = Applicant::find_by_sql($sql);
 ?>
@@ -21,15 +24,22 @@ $applicants = Applicant::find_by_sql($sql);
         <th>Name</th>
         <th>Date Applied</th>
         <th>Delivery Point</th>
+        <?php if (($display == "process_order") || ($display == "deliver")){ ?>
+        <th>Action</th>
+        <?php } ?>
       </tr>
     </thead>
     <tbody>
     <?php foreach ($applicants as $applicant ) { ?>
       <tr>
-        <td><?php echo $applicant->school_id;  ?></td>
+        <td><?php echo $applicant->Id;  ?></td>
         <td><?php echo $applicant->full_name(); ?></td>
         <td><?php echo $applicant->find_application_date(); ?></td>
         <td><?php echo $applicant->find_date_delivered(); ?></td>
+        <?php if (($display == "process_order") || ($display == "deliver")){ ?>
+        <td><a href="<?php echo "?page={$next_page}&id={$applicant->Id}"?>">
+          Process</a></td>
+        <?php } ?>
       </tr>
     <?php } ?>
   </tbody>
