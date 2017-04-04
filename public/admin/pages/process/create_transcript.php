87acbbaec46059af06d0381a668bb18f1e44f6c5
Kenny-Rogers <ylodonu@gmail.com>
2017-04-04 13:17:18 +0000

    adding test transcript file

--- public/admin/pages/process/create_transcript.php
+++ public/admin/pages/process/create_transcript.php
@@ -1,4 +1,16 @@
 <?php
-
-echo "created ";
+//this page creates the Transcript
+//getting the id
+$id = isset($_GET['id'])? $_GET['id'] :'-1';
+$message = "Transcript Successfully Created.";
+$source = "../transcripts/test.pdf";
 ?>
+
+<div class='alert alert-success'>
+  	  <?php echo $message; ?>
+</div>
+<div class="row">
+  <div class="col-sm-offset-1 col-sm-10">
+    <?php echo "<iframe src='{$source}'  style='width: 100%; height:100%;' ></iframe>"; ?>
+  </div>
+</div>
