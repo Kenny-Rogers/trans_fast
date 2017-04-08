47a37c0b9709fed9053c4ea332102cbc9f976fb3
Kenny-Rogers <ylodonu@gmail.com>
2017-04-08 07:13:16 +0000

    adding the function get transcript file location

--- includes/models/transcript.php
+++ includes/models/transcript.php
@@ -2,26 +2,32 @@
 
 class Transcript extends DatabaseObject{
   protected static $table_name = "transcript";
+  protected  $file_loc = "../transcripts/";
   protected static $db_fields =
-    array("trans_id", "file_loc", "generated_for", "generated_by", "date_generated");
-  protected $trans_id;
-  protected $file_loc;
+    array("id", "file_name", "generated_for", "generated_by", "date_generated");
+  protected $id;
+  protected $file_name;
   protected $generated_for;
   protected $generated_by;
   protected $date_generated;
 
-  public static function make($file_loc="", $generated_for="", $generated_by=""){
+  public static function make($file_name, $generated_for, $generated_by){
         //creates the transcript record
-    if !empty($file_loc)&&!empty($generated_for)&&!empty($generated_by) {
-        $transcript = new Transcript();
-        $transcript->file_loc = $file_loc;
-        $transcript->generated_for = $generated_for;
-        $transcript->generated_by = $generated_by;
-        $transcript->date_generated = Strftime("%y-%m-%d %H:%M:%S", time());
-        return $transcript;
+    if (!empty($file_name) && !empty($generated_for) && !empty($generated_by)) {
+        $new_transcript = new Transcript();
+        $new_transcript->file_loc = $file_name;
+        $new_transcript->generated_for = $generated_for;
+        $new_transcript->generated_by = $generated_by;
+        $new_transcript->date_generated = Strftime("%y-%m-%d %H:%M:%S", time());
+        return $new_transcript;
       } else {
         return false;
       }
+  }
+
+  public function get_trans_file(){
+    //return the location of transcript file
+    return "../transcripts/test.pdf";
   }
 }
 ?>
