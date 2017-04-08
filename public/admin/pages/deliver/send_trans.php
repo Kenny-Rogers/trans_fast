62e39ebc5596bd6bc686130f664e1289ec8b0063
Kenny-Rogers <ylodonu@gmail.com>
2017-04-08 07:19:54 +0000

    commenting the send function

--- public/admin/pages/deliver/send_trans.php
+++ public/admin/pages/deliver/send_trans.php
@@ -2,13 +2,13 @@
 $message = "Hello Jacqueline Kumi,Your transcript has been delivered.";
 $number = "233209459533";
 
-$status = Message::send_message($number, $message);
+
+//$status = Message::send_message($number, $message);
 
 if ($status){
-    echo "Transcript delivered successfully.";
+    echo "Transcript delivered successfully.<br>";
 } else {
-  echo "Transcript not delivered .";
+  echo "Transcript not delivered .<br>";
 }
-
 
 ?>
