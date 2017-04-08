ece3ff0c92d4b37b479dbde1802cf5d1db24ff58
Kenny-Rogers <ylodonu@gmail.com>
2017-04-08 16:53:11 +0000

    adding code to send transcript via email to reciepent

--- public/admin/pages/deliver/send_trans.php
+++ public/admin/pages/deliver/send_trans.php
@@ -1,14 +1,33 @@
 <?php
-$message = "Hello Jacqueline Kumi,Your transcript has been delivered.";
+$applicant_id = $_GET['id'];
+$applicant = Applicant::find_by_id($applicant_id);
+$applicant->recipient = Recipient::find_by_id("appl_id={$applicant->id}");
+$applicant->transcript = Transcript::find_by_id("generated_for={$applicant->id}");
+
+$reciever = $applicant->recipient->email();
+$title = "Transcript delivery for ".$applicant->full_name();
+$message = "Hello ,
+  This mail is a transcript from TransFast containing the transcript
+  of ".$applicant->full_name();
+$file = $applicant->transcript->get_trans_file();
+
+$text_message = "Hello Jacqueline Kumi,Your transcript has been delivered.";
 $number = "233209459533";
 
+$email =new Email();
+if ($email->send($reciever, $title, $message, $file)) {
+  $status = Message::send_message($number, $message);
 
-//$status = Message::send_message($number, $message);
-
-if ($status){
-    echo "Transcript delivered successfully.<br>";
+  if ($status){
+    $message = "Transcript Delivered Successfully.";
+    $class = "success";
+  }
 } else {
-  echo "Transcript not delivered .<br>";
+  $message = "Transcript Not Delivered.";
+  $class = "fail";
 }
-
 ?>
+
+<div class="row">
+  <?php echo output_message($message, $class); ?>
+</div>
