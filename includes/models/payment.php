e16ebdfcf63c82a4109efff1fca35235079b5bc4
Kenny-Rogers <ylodonu@gmail.com>
2017-04-08 07:14:39 +0000

    class to store info on payment made by applicant

---
+++ includes/models/payment.php
@@ -0,0 +1,15 @@
+<?php
+//class for Recipients
+class Payment extends DatabaseObject{
+  protected static $table_name = "payment";
+  protected static $db_fields =
+    array("pay_id", "invoice_id", "amount", "paid_by", "date_paid");
+  protected $pay_id;
+  protected $invoice_no;
+  protected $amount;
+  protected $paid_by;
+  protected $date_paid;
+
+
+}
+?>
